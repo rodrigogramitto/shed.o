11eb2204bb9e5b353d0f4defd1d90e2829a4b574
rodrigogramitto <rgramitto@gmail.com>
2023-06-05 12:20:06 -0400

    Basic controller functions added

--- src/db/controller/controller.js
+++ src/db/controller/controller.js
@@ -1,6 +1,22 @@
 const model = require('../models/model');
 
-// eslint-disable-next-line object-shorthand
-const findUser = (email) => model.user.findOne({ email: email });
+const findUser = async (email) => {
+  try {
+    const user = await model.user.findOne({ email });
+    return user;
+  } catch (err) {
+    return err;
+  }
+};
+
+const findRoutine = async (name) => {
+  try {
+    const routine = await model.pushRoutine.findOne({ name });
+    return routine;
+  } catch (err) {
+    return err;
+  }
+};
 
 module.exports.findUser = findUser;
+module.exports.findRoutine = findRoutine;
