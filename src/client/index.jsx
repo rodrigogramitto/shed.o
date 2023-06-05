54c47fa5595ba8e6c498b008a65eaa430a6b37c6
rodrigogramitto <rgramitto@gmail.com>
2023-06-05 17:21:19 -0400

    Main HTML file and App created

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,11 +1,13 @@
+/* eslint-disable import/extensions */
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 // render the App
 import React from 'react';
 import { render } from 'react-dom';
+import App from './components/app.jsx';
 
 const root = document.createElement('div');
 root.setAttribute('id', 'root');
 document.body.appendChild(root);
 
-render(root);
+render(<App />, root);
--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,13 @@
 /* eslint-disable no-console */
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, '../../dist/index.html')));
+app.use(express.json());
 
 try {
   app.listen(3000, () => {
