87f48e948e1f8d8c695662a40887934c1bafe774
rodrigogramitto <rgramitto@gmail.com>
2023-06-06 17:03:59 -0400

    Added authentication

--- src/client/components/app.jsx
+++ src/client/components/app.jsx
@@ -1,17 +1,41 @@
 /* eslint-disable import/extensions */
 import React from 'react';
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  UserButton,
+  useUser,
+  RedirectToSignIn,
+} from '@clerk/clerk-react';
 import MyTimer from './Pomodoro.jsx';
+
+require('dotenv').config();
+
+if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
+  // eslint-disable-next-line no-throw-literal
+  throw 'Missing Publishable Key';
+}
+
+const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
 
 function App() {
   // const time = new Date();
   // time.setSeconds(time.getSeconds() + 1500);
   return (
-    <div>
-      <h1>Shed-O</h1>
-      <div>
-        <MyTimer />
-      </div>
-    </div>
+    <ClerkProvider publishableKey={clerkPubKey}>
+      <SignedIn>
+        <div>
+          <h1>Shed-O</h1>
+          <div>
+            <MyTimer />
+          </div>
+        </div>
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+    </ClerkProvider>
   );
 }
 
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -4,14 +4,6 @@
 // render the App
 import React from 'react';
 import { render } from 'react-dom';
-import {
-  ClerkProvider,
-  SignedIn,
-  SignedOut,
-  UserButton,
-  useUser,
-  RedirectToSignIn,
-} from '@clerk/clerk-react';
 import App from './components/App.jsx';
 
 const root = document.createElement('div');
