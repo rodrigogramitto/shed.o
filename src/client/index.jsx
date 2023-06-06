928a7164e1b28519c9cc12c027aa56706cae1579
rodrigogramitto <rgramitto@gmail.com>
2023-06-06 19:13:33 -0400

    Added authentication

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -3,11 +3,20 @@
 /* eslint-disable no-unused-vars */
 // render the App
 import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App.jsx';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './components/app.jsx';
 
-const root = document.createElement('div');
-root.setAttribute('id', 'root');
-document.body.appendChild(root);
+const root = createRoot(document.getElementById('root'));
 
-render(<App />, root);
+root.render(
+  <Auth0Provider
+    domain="dev-f1b4a5hmp63a60ox.us.auth0.com"
+    clientId="0LjyQVXmSvkGpskx7M2cBNiokbT79a5F"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <App />
+  </Auth0Provider>,
+);
